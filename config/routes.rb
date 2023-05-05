94f8a42dab4233e58ee2800c5cdd1ca196f470e7
Cekula <zafiiboii@gmail.com>
2023-05-05 13:03:54 +0200

    Display stock price in borwser

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-
   devise_scope :user do  
      get '/users/sign_out' => 'devise/sessions#destroy'     
   end
   root 'pages#home'
   get 'my_portfolio', to: 'users#my_portfolio'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'search_stock', to: 'stocks#search'
 end
