422db1a03884b3f78d6b44438c4b3fa3dc782254
Cekula <zafiiboii@gmail.com>
2023-05-05 12:17:11 +0200

    Add stock model and iex api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,11 @@
 class Stock < ApplicationRecord
+
+	def self.new_lookup(ticker_symbol)
+		client = IEX::Api::Client.new(
+		  publishable_token: 'pk_1fdce3179c794932990e38e44f50f3f4',
+		  secret_token: 'sk_61daaf67cdb8429da19ac9122f4f0a9b',
+		  endpoint: 'https://cloud.iexapis.com/v1'
+		)
+	    client.price(ticker_symbol)
+	end
 end
